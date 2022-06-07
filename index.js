c65a7c6c7d088616a122cb63cd58a91cda4453ad
Tahir Hussain <tiger.coding786@gmail.com>
2022-06-07 13:07:58 +0530

    Second Commit: Add Route And Controller for Home

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 
 const port = 8000;
 
+// Use Express Router
+app.use("/", require("./routes"));
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error : ${err}`);
