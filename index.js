3d072e38eaf193de159fec5faa4ba98e5aca95b6
Tahir Hussain <tiger.coding786@gmail.com>
2022-06-07 13:54:29 +0530

    Fourth Commit: Set Up Ejs as View Engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 // Use Express Router
 app.use("/", require("./routes"));
 
+//Set Up EJS as View Engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
 app.listen(port, function (err) {
   if (err) {
     console.log(`Error : ${err}`);
